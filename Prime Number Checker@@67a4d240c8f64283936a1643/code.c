80e6eb86584d51e36cee17a1a0b14998ad407525
Tishya Chugh <tishya0724.becse24@chitkara.edu.in>
2025-03-30 19:13:49 +0530

    Solution to Prime Number Checker

--- Prime Number Checker@@67a4d240c8f64283936a1643/code.c
+++ Prime Number Checker@@67a4d240c8f64283936a1643/code.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <math.h>
 
 int isPrime(int num) {
-    if (num < 2)
-        return 0;
-    for (int i = 2; i * i <= num; i++) {
-        if (num % i == 0)
-            return 0;
+    if (num < 2) return 0;
+    if (num == 2) return 1;  // 2 is the only even prime
+    if (num % 2 == 0) return 0;  // Exclude other even numbers
+
+    for (int i = 3; i * i <= num; i += 2) { // Check odd numbers only
+        if (num % i == 0) return 0;
     }
     return 1;
 }
